e23c17c9093c3f26c5fd09ba0f8af7ff3da8d9ab
mmctyeguy <tyeguy@mmc.school.nz>
2023-08-17 10:16:55 +1200

    set up betting function where users enter an amount between 1-10 to bet on the question

--- bettingv1.py
+++ bettingv1.py
@@ -1,1 +1,16 @@
 error = "please enter a whole number between 1 and 10\n"
+
+valid = False
+while not valid:
+    try:
+        # ask bet amount
+        response = int(input("How much would you like to bet?"))
+        # if amount too high/low give error
+        if 0 < response <= 10:
+            print("you have asked to bet ${}".format(response))
+
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
