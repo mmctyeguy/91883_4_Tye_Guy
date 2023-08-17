bab6e9c679ef68cc535ccfb1311597c78c9173e5
mmctyeguy <tyeguy@mmc.school.nz>
2023-08-18 11:30:24 +1200

    update gamebase1
    contains numcheck

--- gamebase1.py
+++ gamebase1.py
@@ -23,6 +23,24 @@ def instructions():
     return ""
 
 
+def num_check(question, low, high):
+    error = "please enter a whole number between 1 and 10\n"
+    valid = False
+    while not valid:
+        try:
+            # ask question
+            response = int(input(question))
+            # if amount too high/low give error
+            if low < response <= high:
+                return response
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # main
 print("Welcome to generic quiz game")
 
@@ -30,6 +48,8 @@ played_before = yes_no("Have you played before?").lower()
 if played_before == "n" or played_before == "no":
     print(instructions())
     # print instructions if user hasn't played before, otherwise continue program
+
+how_much = num_check("How much would you like to bet?", 0, 10)
+print("You will be betting ${}.".format(how_much))
+
 print("Question one")
-
-
