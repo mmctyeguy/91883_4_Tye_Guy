609df1d84b4429bbff9aa07a522f8f07fdc91e09
mmctyeguy <tyeguy@mmc.school.nz>
2023-08-22 14:18:35 +1200

    update questionsv2

--- questionsv2.py
+++ questionsv2.py
@@ -1,7 +1,40 @@
 import operator
 import random
 
-
+question = 0
+userInput = int()
+answer = str()
+lastName = str()
+firstName = str()
+score = 0
 
 
 def quiz():
+    lastName = input("Please enter your surname:").title()
+    firstName = input("Please enter your first name:").title()
+
+
+for i in range(10):
+    print("What is:")
+    num1 = random.randint(1, 12)
+    num2 = random.randint(1, 12)
+    ops = ['+', '-', '*']
+    operation = random.choice(ops)
+    Q = int(input(str(num1)+operation+str(num2)))
+
+    if ops == '+':
+        answer ==num1+num2
+        if Q == answer:
+            print("Correct")
+            score = score+1
+        else:
+            print("Incorrect")
+
+    elif ops =='-':
+        answer==num1-num2
+        if Q == answer:
+            print("Correct")
+            score = score+1
+        else:
+            print("Incorrect")
+
