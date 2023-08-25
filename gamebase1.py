b177ce70dba37450162e7f517ea260cac0666d1e
mmctyeguy <tyeguy@mmc.school.nz>
2023-08-25 17:34:21 +1200

    update gamebase1
    added in all new functions, minor edits for clarity

--- gamebase1.py
+++ gamebase1.py
@@ -1,3 +1,37 @@
+import operator
+import random
+
+
+def quiz():
+    lastname = input("Please enter your surname:").title()
+    firstname = input("Please enter your first name:").title()
+    print("Your name is {} {}.".format(firstname, lastname))
+    print("Begin Quiz")
+
+
+def calc():
+    score = 0
+    ops = {'+': operator.add,
+           '-': operator.sub,
+           '*': operator.mul,
+           '/': operator.truediv, }
+    num1 = random.randint(1, 12)
+    num2 = random.randint(0, 12)
+    op = random.choice(list(ops.keys()))
+    print('What is {} {} {}?\n'.format(num1, op, num2))
+    q = float(input(str(num1) + op + str(num2)))
+    answer = ops.get(op)(num1, num2)
+
+    if q == answer:
+        print("Correct")
+        score = score + bet
+        print("Your current score is: {}".format(score))
+    else:
+        print("Incorrect")
+        print(answer)
+    return answer
+
+
 def yes_no(question):
     valid = False
     while not valid:
@@ -43,13 +77,26 @@ def num_check(question, low, high):
 
 # main
 print("Welcome to generic quiz game")
+quiz()
 
 played_before = yes_no("Have you played before?").lower()
-if played_before == "n" or played_before == "no":
+if played_before == "no":
     print(instructions())
     # print instructions if user hasn't played before, otherwise continue program
 
 how_much = num_check("How much would you like to bet?", 0, 10)
 print("You will be betting ${}.".format(how_much))
+bet = how_much
 
-print("Question one")
+calc()
+
+next_question = yes_no("Continue to next question?").lower()
+
+if next_question == "yes":
+    print("Next Question")
+    how_much = num_check("How much would you like to bet?", 0, 10)
+    print("You will be betting ${}.".format(how_much))
+    calc()
+
+elif next_question == "no":
+    print("Thank you for playing")
