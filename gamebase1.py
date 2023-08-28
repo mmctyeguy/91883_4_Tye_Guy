f774d282335a11a71d9c59950656245fde99f995
mmctyeguy <tyeguy@mmc.school.nz>
2023-08-28 23:10:43 +1200

    updated gamebase1

--- gamebase1.py
+++ gamebase1.py
@@ -5,8 +5,9 @@ import random
 def quiz():
     lastname = input("Please enter your surname:").title()
     firstname = input("Please enter your first name:").title()
-    print("Your name is {} {}.".format(firstname, lastname))
+    print("Thank you for playing, {} {}.".format(firstname, lastname))
     print("Begin Quiz")
+    # user enters name and it is repeated back to them
 
 
 def calc():
@@ -17,13 +18,18 @@ def calc():
     num1 = random.randint(1, 12)
     num2 = random.randint(0, 12)
     op = random.choice(list(ops.keys()))
-    print('What is {} {} {}?\n'.format(num1, op, num2))
+    print('What is {} {} {}?\n'
+          ''.format(num1, op, num2))
+    # declares ops as the mathematical operations and the numbers in the equation are selected randomly from 1-12
+    # then selects a random operation and puts it into the formula for the questions
 
     try:
         q = int(input())
     except ValueError:
         print("Invalid input. Please enter a number next time.")
         return
+    # if user inputs something other than a number e.g. a letter/symbol an error message is shown
+    # also skips the question
 
     answer = ops.get(op)(num1, num2)
 
@@ -36,6 +42,8 @@ def calc():
         score -= bet
         print(answer)
     return answer
+# if the answer the user inputs is correct, their bet is added to their score
+# otherwise, it is removed from their score.
 
 
 def yes_no(question):
@@ -64,10 +72,11 @@ def instructions():
     print("You may exit the quiz at any time between questions. Good luck")
     print()
     return ""
+    # prints the instructions if called
 
 
 def num_check(question, low, high):
-    error = "please enter a whole number between 1 and 10\n"
+    error = "please enter a whole number between 1 and 10"
     valid = False
     while not valid:
         try:
@@ -76,35 +85,39 @@ def num_check(question, low, high):
             # if amount too high/low give error
             if low < response <= high:
                 return response
-
+                # if the response is an integer between the two numbers declared, it is accepted as the bet
             else:
                 print(error)
 
         except ValueError:
             print(error)
+            # otherwise, an error is printed
 
 
 # main
 print("Welcome to Quiz!")
 quiz()
-
+# calls the quiz function to have the user input their name
 played_before = yes_no("Have you played before?").lower()
 if played_before == "no":
     print(instructions())
     # print instructions if user hasn't played before, otherwise continue program
 bet = num_check("How much would you like to bet?", 0, 10)
 print("You will be betting ${}.".format(bet))
-
+# tells the user how much they are betting
 
 while True:
     calc()
-
+    # starts a loop where the calc function is called
     next_question = yes_no("Next question?").lower()
+    # at the end of the calc function, the user is asked if they wish to continue
 
     if next_question == "yes":
         bet = num_check("How much would you like to bet?", 0, 10)
         print("You will be betting ${}.".format(bet))
         continue
+        # if they answer yes, they continue and are asked to bet again
     elif next_question == "no":
-        print("Thank you for playing")
+        print("Thank you for playing {} {}")
         break
+    # if they say no, the game ends
